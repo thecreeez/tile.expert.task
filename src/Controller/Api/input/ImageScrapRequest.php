bb43183a2d4518ad91514364d07b8ba0daf57c1b
thecreeez <tczdev@gmail.com>
2025-02-21 17:38:55 +0400

    ref: added asserts for request

--- src/Controller/Api/input/ImageScrapRequest.php
+++ src/Controller/Api/input/ImageScrapRequest.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller\Api\input;
 
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[Exclude]
 class ImageScrapRequest
 {
     public function __construct(
+        #[Assert\NotBlank]
         public string $url,
         public string $text,
+
+        #[Assert\PositiveOrZero]
         public int    $minWidth = 0,
+        #[Assert\PositiveOrZero]
         public int    $minHeight = 0,
     )
     {
